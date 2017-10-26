4420ba8f2ee0c1e1dc4dd31f0b5c766d19b082e6
NaveenBen <30779207+NaveenBen@users.noreply.github.com>
2017-10-26 13:57:06 +0000

    Update and rename table.c to 1a.c

---
+++ 1a.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=10;b=13;
+	clrscr();
+	
+	printf("sum of two values is =%d",a+b);
+	printf("subtraction of two values is =%d",a-b);
+	printf("poduct of two values is =%d",a*b);
+	printf("division of two values is =%d",a/b);
+	printf(" moudular division of two values is =%d",a%b);
+	
+	getch()
+	return 0;
+}
+
+/*if u use unix use
+
+cc 1a.c -lm -o 1a
+to get out put ./1a
+
+*/
